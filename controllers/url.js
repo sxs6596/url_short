365ba349d29d4f80e89784b227b437b13bbc7713
sxs6596 <sxs6596@mavs.uta.edu>
2024-08-17 14:36:34 -0500

    second commit

--- controllers/url.js
+++ controllers/url.js
@@ -1,10 +1,11 @@
 const express = require('express'); 
-const {nanoId} = require('nanoid');
+const nanoid = require('nanoid')
+
 
 const URL = require('../models/url'); 
 
 async function handleGenerateNewShortURL(req,res){
-   const shortID = nanoId(8);
+   const shortID = nanoid(8);
    const body = req.body; 
    if(!body.url) return res.status(400).json({error:'url is required'}); 
    await URL.create({
